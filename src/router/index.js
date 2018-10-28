1126a756066b5178ed890f5a72d620079bec7736
ryanxchen <jsryanx@gmail.com>
2018-10-28 23:35:12 +0800

    feat: add react-loadable in router.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,19 @@ import {
   Redirect,
   Switch
 } from 'react-router-dom'
-import NotFound from '@/views/NotFound'
-import Home from '@/views/Home'
-import Getstart from '@/views/Getstart'
-import About from '@/views/About'
+// import NotFound from '@/views/NotFound'
+// import Home from '@/views/Home'
+// import Getstart from '@/views/Getstart'
+// import About from '@/views/About'
 
-// import Loadable from 'react-loadable'
+import Loadable from 'react-loadable'
 
+const LoadableComponent = (component) => {
+  return Loadable({
+    loader: component,
+    loading: () => <span>加载中...</span>,
+  });
+};
 // const DelayLoading = () => (<div>loading ...</div>)
 
 // const Home = Loadable({ loader: () => import('@/views/Home'), loading: DelayLoading, delay: 3000 })
@@ -18,11 +24,22 @@ import About from '@/views/About'
 // const About = Loadable({ loader: () => import('@/views/About'), loading: DelayLoading, delay: 3000 })
 
 const routes = [
+  // { path: '/', exact: true, redirect: '/home' },
+  // { path: '/home', component: 'Home' },
+  // { path: '/getstart', component: 'Getstart' },
+  // { path: '/about', component: 'About' },
+  // { path: '*', component: 'NotFound' }
+
   { path: '/', exact: true, redirect: '/home' },
-  { path: '/home', component: Home },
-  { path: '/getstart', component: Getstart },
-  { path: '/about', component: About },
-  { path: '*', component: NotFound }
+  { path: '/home', component: () => import(`@/views/Home`) },
+  { path: '/getstart', component: () => import(`@/views/Getstart`) },
+  { path: '/about', component: () => import(`@/views/About`) },
+  { path: '*', component: () => import(`@/views/NotFound`) }
+  // { path: '/', exact: true, redirect: '/home' },
+  // { path: '/home', component: Home },
+  // { path: '/getstart', component: Getstart },
+  // { path: '/about', component: About },
+  // { path: '*', component: NotFound }
 ];
 
 export default class extends Component {
@@ -42,7 +59,8 @@ export default class extends Component {
                   key={index}
                   path={route.path}
                   exact={route.exact}
-                  component={route.component}
+                  // component={route.component}
+                  component={LoadableComponent(route.component)}
                 />)
           })
         }
